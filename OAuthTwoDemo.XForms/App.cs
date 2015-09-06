ce96a938672632d9c78c4459d00729328cde743a
Akshay Surve <akshay.surve@gmail.com>
2015-09-06 12:42:41 +0530

    Adds the app id and oauth details

--- OAuthTwoDemo.XForms/App.cs
+++ OAuthTwoDemo.XForms/App.cs
@@ -20,10 +20,10 @@ namespace OAuthTwoDemo.XForms
 							_Instance = new App ();
 							_Instance.OAuthSettings = 
 								new OAuthSettings (
-									clientId: "",  		// your OAuth2 client id 
-									scope: "",  		// The scopes for the particular API you're accessing. The format for this will vary by API.
-									authorizeUrl: "",  	// the auth URL for the service
-									redirectUrl: "");   // the redirect URL for the service
+									clientId: "1639469262962972",  		// your OAuth2 client id 
+									scope: "public_profile,email,user_friends,user_about_me,user_status",  		// The scopes for the particular API you're accessing. The format for this will vary by API.
+									authorizeUrl: "https://m.facebook.com/dialog/oauth/",  	// the auth URL for the service
+									redirectUrl: "http://www.facebook.com/connect/login_success.html");   // the redirect URL for the service
 
 							        // If you'd like to know more about how to integrate with an OAuth provider, 
 									// I personally like the Instagram API docs: http://instagram.com/developer/authentication/
